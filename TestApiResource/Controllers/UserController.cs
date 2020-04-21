ea741f94784ad5e4d3822854f7e4e93e9b88443a
nhdaihn <nhdaihn@gmail.com>
2020-04-21 14:13:11 +0700

    init repository

---
+++ TestApiResource/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApiResource.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetUsers()
+        //{
+        //    return 
+        //}
+    }
+}
